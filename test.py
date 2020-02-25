e9784f94f9d21787bd5b3b4c38d6c57e047925ae
tanmay2625 <pandit.1@iitj.ac.in>
2020-02-25 21:42:54 +0530

    basic zoning

--- test.py
+++ test.py
@@ -6,22 +6,23 @@ import heapq
 import random
 import math
 
-input_img = "im1.png"
-img = cv2.imread(input_img)
+folder="test data"
+input_img = "im20.png"
+img_path=folder+"/"+input_img
+img = cv2.imread(img_path)
 height = img.shape[0]
 width = img.shape[1]
 header_position1 = -1
 header_position2 = -1
 part_projections = []
-offset = 10
-parts = 20
-part_height = -1
+offset = 20
+parts = 40
+part_height=-1
 
 
-def getVerticalProjectionProfile(image, start_position=0, end_position=-1):
-    if end_position == -1:
-        end_position = height
+def getVerticalProjectionProfile(image, start_position=0, end_position=height):
     x = [[0 for i in range(width)] for j in range(height)]
+
     for i in range(start_position, end_position):
         for j in range(width):
             if image[i][j] == 0:
@@ -91,19 +92,19 @@ def removeHeader(img):
     # correctHeader(img,header_position1,header_position2)
     #cv2.imshow('output.png', img)
     # cv2.waitKey(0)
-    cv2.imwrite("rotated_header_removed"+input_img, img)
     return img
 
 
 def getSources():
-    # print(height)
-    full_projection = getVerticalProjectionProfile(img)
+    global part_projections
     sources = []
     l = -1
     for j in range(1, width):
-        if full_projection[j-1] and full_projection[j] == 0:
+        header_part = math.ceil((header_position2+1)/part_height)
+        #print(header_part)
+        if part_projections[header_part][j-1] and part_projections[header_part][j] == 0:
             l = j
-        if full_projection[j] and full_projection[j-1] == 0 and l != -1:
+        if part_projections[header_part][j] and part_projections[header_part][j-1] == 0 and l != -1:
             sources.append(l+(j-l)//2)
     return sources
 
@@ -126,6 +127,7 @@ def getPath(sources):
                     success = 1
                     break
             if success == 0:
+                segments.append([])
                 break
         if success:
             tmp_segment.reverse()
@@ -141,6 +143,7 @@ def getPath(sources):
                         success = 1
                         break
                 if success == 0:
+                    segments.append([])
                     break
             if success:
                 tmp_segment.reverse()
@@ -154,36 +157,34 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img = cropROI(img)
 # cv2.imshow("tmp.jpg",img)
 # cv2.waitKey(0)
-cv2.imwrite("cropped_"+input_img, img)
 height = img.shape[0]
 width = img.shape[1]
 
-
 img = removeHeader(img)
-img = cv2.imread("rotated_header_removed"+input_img)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-(thresh, img) = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
+print(height)
 part_height = math.ceil(height/parts)
+print(part_height)
 for i in range(parts):
     tmp_projection = getVerticalProjectionProfile(
         img, part_height*i, min(part_height*(i+1), height))
     part_projections.append(tmp_projection)
 
-# print(part_projections)
+#print(part_projections)
 
 sources = getSources()
-# print(sources)
+print(sources)
 segments = getPath(sources)
 
-# print(segments)
+print(segments)
 
 tmpimg = img.copy()
 for segment in segments:
-    for k in range(parts):
+    if len(segment)==0:continue
+    for k in range(len(segment)):
         for i in range(part_height*k, min(part_height*(k+1), height)):
             tmpimg[i][segment[k]] = 100
 
 cv2.imshow("tmpfinal.jpg", tmpimg)
 cv2.waitKey(0)
-cv2.imwrite("final_parts_seg_"+input_img, tmpimg)
+cv2.imwrite("results2/final_parts_seg_"+input_img, tmpimg)
